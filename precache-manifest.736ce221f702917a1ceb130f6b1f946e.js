2cbbe5aa862e0270a0d64bef4b6ff60cdc9796f6
Shane Ding <s44ding@uwaterloo.ca>
2020-09-14 16:04:55 -0400

    Updates

---
+++ precache-manifest.736ce221f702917a1ceb130f6b1f946e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34a1bfa944a72c43f00d7adf459cb6f",
+    "url": "/anlessini-demos/index.html"
+  },
+  {
+    "revision": "70f51421f74a8756ecfb",
+    "url": "/anlessini-demos/static/css/main.3bb48105.chunk.css"
+  },
+  {
+    "revision": "2295d800c30b3d271ba1",
+    "url": "/anlessini-demos/static/js/2.d98d4414.chunk.js"
+  },
+  {
+    "revision": "3205f2f97be94286aaab611b41fbc37d",
+    "url": "/anlessini-demos/static/js/2.d98d4414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f51421f74a8756ecfb",
+    "url": "/anlessini-demos/static/js/main.564be3b4.chunk.js"
+  },
+  {
+    "revision": "be193a8fe693c348110b",
+    "url": "/anlessini-demos/static/js/runtime-main.753d0764.js"
+  },
+  {
+    "revision": "78ad86eaa49a636e112ea88b2b20e7f2",
+    "url": "/anlessini-demos/static/media/nyu.78ad86ea.png"
+  },
+  {
+    "revision": "184494226277ae1734d50522c97394e3",
+    "url": "/anlessini-demos/static/media/uwaterloo.18449422.png"
+  }
+]);
